d1f9516f5433928ce42cc787918f3c73a3ad24c5
Samiksha-Pansare <pansaresamiksha@gmail.com>
2022-11-09 18:11:44 +0530

    Deploying to render

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-app.get('/', (req, res) => {
-  res.send("Hello to Memories API!")
-});
+// app.get('/', (req, res) => {
+//   res.send("Hello to Memories API!")
+// });
 
 // const CONNECTION_URL = "mongodb+srv://sam:sam123@cluster0.ifowfwg.mongodb.net/?retryWrites=true&w=majority";
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
