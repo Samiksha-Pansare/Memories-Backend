7575b5b1b1177a9130f59d1bd9f65db604cef5a1
Samiksha-Pansare <pansaresamiksha@gmail.com>
2022-11-20 12:58:56 +0530

    deployment changes

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ dotenv.config()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000","https://memories-app.onrender.com"]
+}));
 
 
 app.use('/posts', postRoutes);
